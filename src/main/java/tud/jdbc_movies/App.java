676ec43e86cffef169b2f68ef2ead1c1eccf808e
aonych <aleksandrao995@gmail.com>
2016-11-16 16:25:57 +0100

    Rozpoczęcie projektu

---
+++ src/main/java/tud/jdbc_movies/App.java
@@ -0,0 +1,13 @@
+package tud.jdbc_movies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
